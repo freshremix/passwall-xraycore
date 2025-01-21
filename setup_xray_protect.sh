a734e7b32a62b165590c3ecc8b25394c71312704
freshremix <90133873+freshremix@users.noreply.github.com>
2025-01-21 06:52:50 +0330

    Update setup_xray_protect.sh

--- setup_xray_protect.sh
+++ setup_xray_protect.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
-cat <<EOF > /etc/init.d/xray_protect
+cat <<'EOF' > /etc/init.d/xray_protect
 START=99
 stop() {
-    # Uncomment this if you want to stop xray manually with the service
-    # killall xray
+killall xray
 }
 start() {
-    while true; do
-        pid=\$(pidof xray)
-        if [ -n "\$pid" ]; then
-            echo -1000 > /proc/\$pid/oom_score_adj
-        else
-            logger "xray process not running"
-        fi
-        sleep 5
-    done
+while true; do
+pid=$(pidof xray)
+if [ -n "$pid" ]; then
+echo -1000 > /proc/$pid/oom_score_adj
+else
+logger "xray process not running"
+fi
+sleep 5
+done
 }
 EOF
 chmod +x /etc/init.d/xray_protect
 /etc/init.d/xray_protect enable
 /etc/init.d/xray_protect start
 /etc/init.d/xray_protect status
-cat /proc/\$(pidof xray)/oom_score_adj
+cat /proc/$(pidof xray)/oom_score_adj
