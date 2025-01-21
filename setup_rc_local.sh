c347415818ebdedbe7fa6025724d6afe4b6cf3a6
freshremix <90133873+freshremix@users.noreply.github.com>
2025-01-21 23:41:56 +0330

    Update setup_rc_local.sh

--- setup_rc_local.sh
+++ setup_rc_local.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+touch /etc/rc.local
+chmod +x /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+/etc/init.d/rc.local enable
+/etc/init.d/rc.local start
 echo '#!/bin/sh' > /tmp/rc_local_script.sh
 echo 'echo -1 > /proc/$(pgrep xray)/oom_score_adj' >> /tmp/rc_local_script.sh
 echo 'exit 0' >> /tmp/rc_local_script.sh
